006121a762f826090790c7d0c6eab2cdeeb897f6
GregCahillC00216328 <C00216328@itcarlow.ie>
2017-12-07 13:01:21 +0000

    first commit

---
+++ MuddyGames-gameplay-programming-i-lab-05-1f0d12c136d6/SFMLOpenGL/Game.cpp
@@ -0,0 +1,110 @@
+#include <Game.h>
+
+Game::Game() : window(VideoMode(800, 600), "OpenGL")
+{
+}
+
+Game::~Game() {}
+
+void Game::run()
+{
+
+	initialize();
+
+	Event event;
+
+	while (isRunning) {
+
+		cout << "Game running..." << endl;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				isRunning = false;
+			}
+		}
+		update();
+		draw();
+	}
+
+}
+
+void Game::initialize()
+{
+	isRunning = true;
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0); 
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void Game::update()
+{
+	cout << "Update up" << endl;
+}
+
+void Game::draw()
+{
+	 cout << "Draw up" << endl; 
+	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	
+	 glBegin(GL_LINES);
+	 { 
+		 glColor3f(255, 255, 255);
+	 glVertex3f(0.0, 2.0, -10.0); 
+	 glColor3f(255, 255, 255);
+	 glVertex3f(-2.0, -2.0, -10.0);
+	 }
+	 glEnd(); 
+	 glBegin(GL_LINES);
+	 {
+		 glColor3f(255, 255, 255);
+		 glVertex3f(-2.0, -2.0, -10.0);
+		 glColor3f(255, 255, 255);
+		 glVertex3f(2.0, -2.0, -10.0); }
+	 glEnd();
+	 glBegin(GL_LINES);
+	 {
+		 glColor3f(255, 255, 255);
+		 glVertex3f(0.0, 2.0, -10.0);
+		 glColor3f(255, 255, 255);
+		 glVertex3f(2.0, -2.0, -10.0); }
+	 glEnd();
+	 glBegin(GL_LINES);
+	 {
+		 glColor3f(255, 255, 255);
+		 glVertex3f(-.5, .0, -5.0);
+		 glColor3f(255, 255, 255);
+		 glVertex3f(-2.2, -.7, -5.0); }
+	 glEnd();
+	 glBegin(GL_TRIANGLES); 
+	 { 
+		 glColor3f(255, 255, 255);
+	 glVertex3f(-.5, .0, -5); 
+	 glColor3f(0, 0, 0);
+	 glVertex3f(.5, .0, -5);
+	 glColor3f(5, 5, 5);
+	 glVertex3f(.3, .4, -5); 
+	 }
+	 
+	 glEnd();
+	 glBegin(GL_QUADS);
+	 { 
+		 glColor3f(0, 0, 255);
+	 glVertex3f(3, -1., -5);
+	 glColor3f(0, 255, 0);
+	 glVertex3f(.5, .0, -5);
+	 glColor3f(255, 0, 0);
+	 glVertex3f(.3, .4, -5);
+	 glColor3f(255, 0, 255);
+	 glVertex3f(3, -.1, -5); }
+	 glEnd();
+	 window.display(); 
+}
+
+void Game::unload()
+{
+	cout << "Cleaning up" << endl;
+}
+
---
+++ MuddyGames-gameplay-programming-i-lab-05-1f0d12c136d6/SFMLOpenGL/Game.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+using namespace std;
+using namespace sf;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void draw();
+	void unload();
+
+	enum states
+	{
+		Points,
+		Lines,
+		Line_Strip,
+		Line_Loop,
+		Triangles,
+		Triangle_Strip,
+		Triangle_Fan,
+		Quads,
+		Quad_Strip,
+		Polygon
+	};
+
+	// // Uncomment for Part 2
+	// // ********************
+	// const int primatives;
+
+	//GLuint index;
+	//Clock clock;
+	//Time elapsed;
+
+	//float rotationAngle = 0.0f;
+	// // ********************
+};
